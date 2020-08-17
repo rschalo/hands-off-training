cd08bb0874487c2b7461b3ea0dc87e28d351f2af
Reed Schalo <reed0schalo@gmail.com>
2020-08-17 16:29:01 -0700

    feat: hash passwords and POST newUser

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
 const passportLocalMongoose = require('passport-local-mongoose');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
@@ -29,6 +30,26 @@ const userSchema = new Schema(
     timestamps: true
   }
 );
+
+userSchema.methods = {
+  checkPassword: function (inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.password);
+  },
+  hashPassword: (plainTextPassword) => {
+    return bcrypt.hashSync(plainTextPassword, 10);
+  }
+};
+
+userSchema.pre('save', function (next) {
+  if (!this.password) {
+    console.log('models/user.js =======NO PASSWORD PROVIDED=======');
+    next();
+  } else {
+    console.log('models/user.js hashPassword in pre save');
+    this.password = this.hashPassword(this.password);
+    next();
+  }
+});
 
 userSchema.plugin(passportLocalMongoose);
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const passport = require('passport');
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
@@ -11,18 +10,10 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-
   const newUser = new User({ username, password });
-  User.register(newUser, (err, user) => {
-    try {
-      passport.authenticate('local')(req, res, () => {
-        res.json('User added!');
-        console.log(user);
-      });
-    } catch (err) {
-      res.status(400).json('Error: ' + err);
-      console.error(err);
-    }
+  newUser.save((err, user) => {
+    if (err) return res.json(err);
+    return res.json(user);
   });
 });
 
