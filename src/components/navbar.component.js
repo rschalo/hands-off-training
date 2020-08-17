fcd9f04b26dc45ebc086b41bf57d6fb78de4039d
Reed Schalo <reed0schalo@gmail.com>
2020-08-17 16:32:30 -0700

    feat: update headers to this project

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,18 +6,18 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">
-          ExcerTracker
+          Hands Off Training
         </Link>
         <div className="collpase navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
               <Link to="/" className="nav-link">
-                Exercises
+                Workout Classes
               </Link>
             </li>
             <li className="navbar-item">
               <Link to="/create" className="nav-link">
-                Create Exercise Log
+                Create Workout Class
               </Link>
             </li>
             <li className="navbar-item">
