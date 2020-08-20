e70e8bc01fd689dca65d2900702e1b5b04784b67
Reed Schalo <reed0schalo@gmail.com>
2020-08-20 13:56:25 -0700

    feat: add route for logout

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -20,6 +20,24 @@ router.route('/signup').post((req, res) => {
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
   console.log(`${req.body.username} has logged in!`);
+  const user = JSON.parse(JSON.stringify(req.user));
+  const cleanUser = Object.assign({}, user);
+  if (cleanUser.local) {
+    console.log(`Deleting ${cleanUser.local.password}`);
+    delete cleanUser.local.password;
+  }
+  res.json({ user: cleanUser });
+  res.locals.username = req.body.username;
+});
+
+router.post('/logout', (req, res) => {
+  if (req.user) {
+    req.session.destroy();
+    res.clearCookie('connect.sid'); // clean up!
+    return res.json({ msg: 'logging you out' });
+  } else {
+    return res.json({ msg: 'no user to log out!' });
+  }
 });
 
 module.exports = router;
