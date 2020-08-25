b59e292940866150ecb9824339103e126a3e16c2
Reed Schalo <reed0schalo@gmail.com>
2020-08-24 17:08:36 -0700

    feat: add auto toggle to menu when collapsed

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      button: 'collapsed',
+      ariaExpanded: 'false',
+      topDiv: 'collapse'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.button === 'collapsed') {
+      this.setState({
+        button: '',
+        ariaExpanded: 'true',
+        topDiv: 'show'
+      });
+    } else {
+      this.setState({
+        button: 'collapsed',
+        ariaExpanded: 'false',
+        topDiv: 'collapse'
+      });
+    }
+  }
   render() {
     let loggedInStatus;
     if (this.props.loggedIn) {
@@ -15,50 +40,60 @@ export default class Navbar extends Component {
     } else {
       loggedInStatus = (
         <li className="navbar-item">
-          <Link to="/login" className="nav-link">
+          <Link to="/login" className="nav-link" onClick={this.handleClick}>
             Login
           </Link>
         </li>
       );
     }
+    let createClass;
     let userName;
     if (this.props.userName !== null) {
       userName = (
         <li className="navbar-item">
-          <Link to="/signup" className="nav-link">
+          <Link to="/signup" className="nav-link" onClick={this.handleClick}>
             Sign Up
+          </Link>
+        </li>
+      );
+    }
+    if (this.props.username === 'admin') {
+      createClass = (
+        <li className="navbar-item">
+          <Link to="/create" className="nav-link" onClick={this.handleClick}>
+            Create Workout Class
           </Link>
         </li>
       );
     }
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">
+        <Link to="/" className="navbar-brand" onClick={this.handleClick}>
           Hands Off Training
         </Link>
         <button
-          className="navbar-toggler"
+          className={`navbar-toggler ${this.state.button}`}
           type="button"
           data-toggle="collapse"
           data-target="#navbarToggler"
           aria-controls="navbarToggler"
-          aria-expanded="false"
+          aria-expanded={this.state.ariaExpanded}
           aria-label="Toggle navigation"
+          onClick={this.handleClick}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="navbar-collapse collapse" id="navbarToggler">
+        <div
+          className={`navbar-collapse ${this.state.topDiv}`}
+          id="navbarToggler"
+        >
           <ul className="navbar-nav ml-auto">
             <li className="navbar-item">
-              <Link to="/" className="nav-link">
+              <Link to="/" className="nav-link" onClick={this.handleClick}>
                 Workout Classes
               </Link>
             </li>
-            <li className="navbar-item">
-              <Link to="/create" className="nav-link">
-                Create Workout Class
-              </Link>
-            </li>
+            {createClass}
             {userName}
             {loggedInStatus}
           </ul>
